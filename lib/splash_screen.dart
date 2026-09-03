d882b22314e9e146385856794ac442f7ffa75f42
oritri98 <ismatsiddiquee98@gmail.com>
2026-09-04 00:07:20 +0600

    Add splash_screen.dart

---
+++ lib/splash_screen.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:splash_screen_ori/homePage.dart';
+import 'package:lottie/lottie.dart';
+
+class AnimatedSplashScreenWidget extends StatefulWidget {
+  const AnimatedSplashScreenWidget({super.key});
+
+  @override
+  State<AnimatedSplashScreenWidget> createState() => _AnimatedSplashScreenWidgetState();
+}
+
+class _AnimatedSplashScreenWidgetState extends State<AnimatedSplashScreenWidget> {
+  Timer? _timer;
+
+ 
+
+
+}
